8113602f12516d562c42198646bc1b781b4f0990
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-11-13 08:56:00 -0300

    feat. Utils class, printLine and toUppercase methods implementation

--- src/main/java/org/example/util/Util.java
+++ src/main/java/org/example/util/Util.java
@@ -1,4 +1,11 @@
 package org.example.util;
 
 public class Util {
+    public static void printLine() {
+        System.out.println("-----------------------------------------------------");
+    }
+
+    public static String toUppercase(String text) {
+        return text.toUpperCase();
+    }
 }
