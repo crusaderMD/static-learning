71774399b494749110ef510341f3a05f26096a15
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-11-13 09:04:54 -0300

    feat. Users class, showInfo and showTotalUsers implementation

--- src/main/java/org/example/entities/User.java
+++ src/main/java/org/example/entities/User.java
@@ -1,6 +1,21 @@
 package org.example.entities;
 
 public class User {
+    private static int nextId = 1;
 
+    private int id;
+    private String name;
 
+    public User(String name) {
+        this.id = nextId++;
+        this.name = name;
+    }
+
+    public void showInfo() {
+        System.out.println("ID: " + id + " | Name: " + name);
+    }
+
+    public static  void showTotalUsers() {
+        System.out.println("Created users total: " + (nextId - 1));
+    }
 }
